2ba4193bd1cdd0e44af90acd7515c78330bccc5c
David <dweke12@gmail.com>
2023-05-18 13:21:01 +0300

    again commit

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -7,7 +7,7 @@ const Balance = () => {
   const total = amount.reduce((acc, item) => (acc += item), 0).toFixed(2);
   return (
     <div>
-      <h4>Your Balance is here</h4>
+      <h4>Your Balance is here again</h4>
       <h1>${total}</h1>
     </div>
   );
